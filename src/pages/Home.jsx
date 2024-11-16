cf2518702edee36908d798b3a4bf78df886ac000
Jeetaksh25 <manojjeetakshluhaniwal25@gmail.com>
2024-11-16 17:49:53 +0530

    Online Fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,7 +31,7 @@ const Home = () => {
       <h1 align="center" className='MainHeading'>Maven, Your Friend</h1>
       <div className='intro'>
         <div className='introL'>
-          <img src="/public/Content/images/Maven.png" alt="Maven" className='Mavenimg' />
+          <img src="https://raw.githubusercontent.com/Jeetaksh25/Maven/refs/heads/main/public/Content/images/Maven.png" alt="Maven" className='Mavenimg' />
         </div>
         <div className='introR'>
           <h1 data-aos="fade-right">Welcome meowy friend!!</h1>
@@ -75,7 +75,7 @@ const Home = () => {
 
     <div class="Types">
         <div class="Type">
-            <img src="./public/Content/images2/Anxiety.png" class="TypeI" width="200px" data-aos="flip-left" loading='lazy'/>
+            <img src="https://raw.githubusercontent.com/Jeetaksh25/Maven/refs/heads/main/public/Content/images2/Anxiety.png" class="TypeI" width="200px" data-aos="flip-left" loading='lazy'/>
             <p>Anxiety Disorder</p>
             <div className='description'>
               <p style={{color:"white"}}>Anxiety disorders are the most common mental health conditions. They are characterized by persistent and excessive fear or worry about various aspects of life. This overwhelming anxiety can interfere with daily activities and often leads to physical symptoms such as rapid heartbeat, sweating, and fatigue.</p>
@@ -83,7 +83,7 @@ const Home = () => {
         </div>
 
         <div class="Type">
-            <img src="./public/Content/images2/Mood.png" class="TypeI" width="200px" data-aos="flip-up" loading='lazy'/>
+            <img src="https://raw.githubusercontent.com/Jeetaksh25/Maven/refs/heads/main/public/Content/images2/Mood.png" class="TypeI" width="200px" data-aos="flip-up" loading='lazy'/>
             <p>Mood Disorder</p>
             <div className='description'>
               <p style={{color:"white"}}> Mood disorders primarily involve disturbances in a person's mood. This includes conditions like depression and bipolar disorder. Individuals with mood disorders may experience prolonged feelings of sadness, irritability, or extreme mood swings, which can significantly impact their quality of life.</p>
@@ -91,7 +91,7 @@ const Home = () => {
         </div>
 
         <div class="Type">
-            <img src="./public/Content/images2/Psychotic.png" class="TypeI" width="200px" data-aos="flip-right" loading='lazy'/>
+            <img src="https://raw.githubusercontent.com/Jeetaksh25/Maven/refs/heads/main/public/Content/images2/Psychotic.png" class="TypeI" width="200px" data-aos="flip-right" loading='lazy'/>
             <p>Psychotic Disorder</p>
             <div className='description'>
               <p style={{color:"white"}}>Psychotic disorders involve distorted thinking and perceptions, which can include hallucinations (seeing or hearing things that aren't there) and delusions (strong beliefs that are not based in reality). These disorders can severely affect a person’s ability to function and interact with others.</p>
@@ -99,7 +99,7 @@ const Home = () => {
         </div>
 
         <div class="Type">
-            <img src="./public/Content/images2/Personality.png" class="TypeI" width="200px" data-aos="flip-left" loading='lazy'/>
+            <img src="https://raw.githubusercontent.com/Jeetaksh25/Maven/refs/heads/main/public/Content/images2/Personality.png" class="TypeI" width="200px" data-aos="flip-left" loading='lazy'/>
             <p>Personality Disorder</p>
             <div className='description'>
               <p style={{color:"white"}}>Personality disorders are characterized by enduring patterns of behavior, cognition, and inner experience that deviate from cultural expectations. These patterns can lead to significant impairment or distress, affecting relationships and the ability to function in everyday life.</p>
@@ -107,7 +107,7 @@ const Home = () => {
         </div>
 
         <div class="Type">
-            <img src="./public/Content/images2/Eating.png" class="TypeI" width="200px" data-aos="flip-up" loading='lazy'/>
+            <img src="https://raw.githubusercontent.com/Jeetaksh25/Maven/refs/heads/main/public/Content/images2/Eating.png" class="TypeI" width="200px" data-aos="flip-up" loading='lazy'/>
             <p>Eating Disorder</p>
             <div className='description'>
               <p style={{color:"white"}}>Eating disorders encompass a range of conditions that involve unhealthy eating habits and attitudes towards food, body weight, and body image. Conditions such as anorexia nervosa, bulimia nervosa, and binge-eating disorder can lead to severe health complications and emotional distress.</p>
@@ -115,7 +115,7 @@ const Home = () => {
         </div>
 
         <div class="Type">
-            <img src="./public/Content/images2/Trauma-related.png" class="TypeI" width="200px" data-aos="flip-right" loading='lazy'/>
+            <img src="https://raw.githubusercontent.com/Jeetaksh25/Maven/refs/heads/main/public/Content/images2/Trauma-related.png" class="TypeI" width="200px" data-aos="flip-right" loading='lazy'/>
             <p>Trauma-Related Disorder</p>
             <div className='description'>
               <p style={{color:"white"}}>Trauma-related disorders occur in response to experiencing or witnessing a traumatic event. Post-Traumatic Stress Disorder (PTSD) is a common example, where individuals may relive the trauma through flashbacks or nightmares, often leading to anxiety and emotional distress.</p>
@@ -123,7 +123,7 @@ const Home = () => {
         </div>
 
         <div class="Type">
-            <img src="./public/Content/images2/Substance-related-Addictive.png" class="TypeI" width="200px" data-aos="flip-left" loading='lazy'/>
+            <img src="https://raw.githubusercontent.com/Jeetaksh25/Maven/refs/heads/main/public/Content/images2/Substance-related-Addictive.png" class="TypeI" width="200px" data-aos="flip-left" loading='lazy'/>
             <p>Addictive Disorder</p>
             <div className='description'>
               <p style={{color:"white"}}>This type of disorder involves the harmful use of psychoactive substances, including alcohol and drugs, leading to significant impairment or distress. These disorders can cause physical, psychological, and social issues, making it challenging for individuals to manage their lives.</p>
@@ -131,7 +131,7 @@ const Home = () => {
         </div>
 
         <div class="Type">
-            <img src="./public/Content/images2/Neurodevelopment.png" class="TypeI" width="200px" data-aos="flip-up" loading='lazy'/>
+            <img src="https://raw.githubusercontent.com/Jeetaksh25/Maven/refs/heads/main/public/Content/images2/Neurodevelopment.png" class="TypeI" width="200px" data-aos="flip-up" loading='lazy'/>
             <p>Neurodevelopment Disorder</p>
             <div className='description'>
               <p style={{color:"white"}}>Neurodevelopmental disorders typically manifest in early childhood and affect how the brain develops. Conditions such as Autism Spectrum Disorder (ASD) and Attention-Deficit/Hyperactivity Disorder (ADHD) can affect cognitive, social, and emotional functioning.</p>
@@ -139,7 +139,7 @@ const Home = () => {
         </div>
 
         <div class="Type">
-            <img src="./public/Content/images2/Dissociative.png" class="TypeI" width="200px" data-aos="flip-right" loading='lazy'/>
+            <img src="https://raw.githubusercontent.com/Jeetaksh25/Maven/refs/heads/main/public/Content/images2/Dissociative.png" class="TypeI" width="200px" data-aos="flip-right" loading='lazy'/>
             <p>Dissociative Disorder</p>
             <div className='description'>
               <p style={{color:"white"}}>Dissociative disorders involve a disconnection between thoughts, identity, consciousness, and memory. Individuals may experience a detachment from their thoughts and sense of self, often as a coping mechanism in response to trauma.</p>
@@ -147,7 +147,7 @@ const Home = () => {
         </div>
 
         <div class="Type">
-            <img src="./public/Content/images2/Somatoform.png" class="TypeI" width="200px" data-aos="flip-left" loading='lazy'/>
+            <img src="https://raw.githubusercontent.com/Jeetaksh25/Maven/refs/heads/main/public/Content/images2/Somatoform.png" class="TypeI" width="200px" data-aos="flip-left" loading='lazy'/>
             <p>Somatoform Disorder</p>
             <div className='description'>
               <p style={{color:"white"}}>Somatoform disorders are characterized by physical symptoms that cannot be fully explained by a medical condition. Individuals may experience significant distress and impairment due to these symptoms, which can lead to frequent medical visits and anxiety.</p>
@@ -155,7 +155,7 @@ const Home = () => {
         </div>
 
         <div class="Type">
-            <img src="./public/Content/images2/Sleep.png" class="TypeI" width="200px" data-aos="flip-up" loading='lazy'/>
+            <img src="https://raw.githubusercontent.com/Jeetaksh25/Maven/refs/heads/main/public/Content/images2/Sleep.png" class="TypeI" width="200px" data-aos="flip-up" loading='lazy'/>
             <p>Sleep Disorder</p>
             <div className='description'>
               <p style={{color:"white"}}>Sleep disorders encompass a range of conditions that affect the ability to sleep well on a regular basis. Insomnia, sleep apnea, and narcolepsy are examples that can lead to daytime fatigue, mood disturbances, and health complications.</p>
@@ -163,7 +163,7 @@ const Home = () => {
         </div>
 
         <div class="Type">
-            <img src="./public/Content/images2/Neurocognitive.png" class="TypeI" width="200px" data-aos="flip-right" loading='lazy'/>
+            <img src="https://raw.githubusercontent.com/Jeetaksh25/Maven/refs/heads/main/public/Content/images2/Neurocognitive.png" class="TypeI" width="200px" data-aos="flip-right" loading='lazy'/>
             <p>Neurocognitive Disorder</p>
             <div className='description'>
               <p style={{color:"white"}}>Neurocognitive disorders primarily affect cognitive functions such as memory, attention, and problem-solving. Dementia and Delirium are examples, often resulting from injury, disease, or substance use, leading to significant impairments in daily living.</p>
